c4503e6a3704b4b4c41b6bddf0ce7500e16688f2
Rajesh Koirala <rajeshkoirala055@gmail.com>
2025-08-26 11:40:06 +0545

    add number

---
+++ sum_Twonumber.py
@@ -0,0 +1,8 @@
+# 2. A program that asks for two numbers and prints their sum.
+
+num1=int (input("Enter the first number"))
+num2=int (input("Enter the second number"))
+
+sum=(num1+num2)
+
+print("The sum of two number is:",sum)
