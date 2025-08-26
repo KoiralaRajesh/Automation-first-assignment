720c29bd6def06edf208e54b7e7b1184347a58c4
Rajesh Koirala <rajeshkoirala055@gmail.com>
2025-08-26 11:45:28 +0545

    list of fruits

---
+++ list.py
@@ -0,0 +1,6 @@
+# 3. A program that stores a few fruits in a list and prints them one by one.
+
+fruits = ["Banana", "orange", "papaya", "gava"]
+print("The fruits in the list is :")
+for fruit in fruits:
+    print(fruit)
